79af10941186310650991d87d81aea5eafc31a78
Tyler Chen <chentylerl@gmail.com>
2014-12-21 10:42:53 -0500

    basic setup

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,8 @@
-//smooth scroll
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
 
-
+//colors: [text, background]
 function switcher( e ){
-var ifr = document.getElementById( 'p_iframe' );
-
-ifr.src = './projects/' + e + '/index.html'; 
-
+  var ifr = document.getElementById( 'p_iframe' );
+  ifr.src = './projects/' + e + '/index.html'; 
+  document.getElementById( 'menu_container' ).className = e;
+  
 }
