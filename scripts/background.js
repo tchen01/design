66db64721e08ede8ace38c6af39983c6ff0a8b4a
Tyler Chen <chentylerl@gmail.com>
2015-02-26 21:53:03 -0500

    but nothing clicks

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,7 @@
+document.getElementById('header').classList.add( 'expanded' );
 var scroll = document.getElementById( 'scroll' );
 var words = "create build destroy live make love craft <b>ART</b>".split(" ");
 
-function write(w){
-}
 
 var i=1
 
@@ -19,5 +18,18 @@ function wordLoop(){
             scroll.innerText = "";
             document.getElementById( 'header' ).classList.remove( 'expanded');
         }
-    }, 210)
+    }, 00)
+}
+
+var timeout = setInterval(focuser, 6000);
+document.onmousemove = function(){
+  clearInterval(timeout);
+  timeout = setInterval(focuser, 6000);
+}
+
+var images = document.getElementsByClassName( 'image' );
+function focuser(){
+    var n = Math.floor(Math.random() * images.length);
+    images[n].firstChild.classList.add( 'focus' );
+    setTimeout(function(){images[n].firstChild.classList.remove( 'focus' );}, 2000)
 }
