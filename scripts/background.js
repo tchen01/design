328bb9258d54c3bbc411e1eb08c392f2981dc080
Tyler Chen <chentylerl@gmail.com>
2016-01-01 15:08:02 -0600

    o wao

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,5 @@
 
-var scrollText = document.getElementById( 'scroll' );
+var scrollText = document.querySelector( '#scroll' );
 var words = "create build destroy live make love craft <b>ART</b> ".split(" ");
 var times = [350, 180, 180, 240, 130, 250, 250, 330, 1000]
 var i=1;
@@ -7,10 +7,10 @@ if( document.referrer.indexOf(window.location.host) === -1 ){
       //intro();
 }
 function intro(){
-    if( document.getElementById('header').classList.length == 0 ){
+    if( document.querySelector('#header').classList.length == 0 ){
         i=1;
         console.log(i);
-        document.getElementById('header').classList.add( 'expanded' );
+        document.querySelector('#header').classList.add( 'expanded' );
 
         scrollText.innerText = words[0] + " ";
         wordLoop();
@@ -26,17 +26,22 @@ function wordLoop(){
             wordLoop();   
         } else {
             scrollText.textContent = '';
-            document.getElementById( 'header' ).classList.remove( 'expanded');
+            document.querySelector( '#header' ).classList.remove( 'expanded');
         }
     }, times[i-1])
 }
 
-function show( type ){
-    elements = document.getElementsByClassName("item");
+var menu = document.querySelectorAll(".type");
+function show( t, type ){
+    for(i=0; i<menu.length; i++){
+        menu[i].classList.remove("focus");
+    }
+    t.classList.add("focus");
+    elements = document.querySelectorAll(".item");
     for(i=0; i<elements.length; i++){
         elements[i].classList.add("hidden");
     }
-    if( type !== "all" ){
+    if( type !== "" ){
         elements = document.getElementsByClassName( type );
     }
     for(i=0; i<elements.length; i++){
@@ -60,7 +65,7 @@ document.onmousemove = function(){
   timeout = setInterval(focuser, 6000);
 }
 
-var images = document.getElementsByClassName( 'item' );
+var images = document.querySelectorAll( '.item' );
 function focuser(){
     var n = Math.floor(Math.random() * images.length);
     images[n].firstChild.classList.add( 'focus' );
--- scripts/pages.js
+++ scripts/pages.js
@@ -1,8 +1,28 @@
-var images = document.getElementsByTagName('img');
+var images = document.querySelectorAll('img');
+var cover = document.querySelector('#cover');
 for(i=0; i<images.length; i++){
-    images[i].addEventListener('click', toggle);
+    images[i].addEventListener('click', enlarge);
 }
 
-function toggle(){
-    this.classList.toggle('large'); 
+function enlarge(e){
+    e.stopPropagation();
+    /*for(i=0; i<images.length; i++){
+        if( this != images[i] ){
+            images[i].classList.remove('large');
+        }
+    }*/
+    this.classList.toggle('large');
+    cover.classList.toggle('expanded');
+    document.addEventListener('click', shrink);
+}
+
+document.addEventListener('keydown', shrink);
+function shrink(e){
+    e.stopPropagation();
+    if( e.keyCode == undefined || e.keyCode == 27){
+        for(i=0; i<images.length; i++){
+           images[i].classList.remove('large');
+        }
+        cover.classList.remove('expanded');
+    }
 }
