588ec944ecb484a3905c97fd672e3073bc2122df
Tyler Chen <chentylerl@gmail.com>
2016-12-28 21:37:47 -0600

    cleaning up

--- old/scripts/background.js
+++
@@ -1,8 +0,0 @@
-
-//colors: [text, background]
-function switcher( e ){
-  var ifr = document.getElementById( 'p_iframe' );
-  ifr.src = './projects/' + e + '/index.html'; 
-  document.getElementById( 'menu_container' ).className = e;
-  
-}
